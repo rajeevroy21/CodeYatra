1f4da34cf24f83eaec1a7034062c7b53d2355ddf
Rajeev kumar <rajeevroy70701@gmail.com>
2025-04-23 01:27:42 +0530

    update code

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://code-yatra-9bdu.vercel.app"],
   credentials: true,
 }));
 
